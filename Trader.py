693dd543d994284eafd92378c9611e720614fc60
Kopano Monyobo <59963220+kopano-m@users.noreply.github.com>
2020-08-28 11:55:13 +0200

    Added 2nd Strategy

--- Trader.py
+++ Trader.py
@@ -7,22 +7,38 @@ import sys
 
 import backtrader as bt
 
-from strategies.tests import TestStrategy
+from strategies.tests import TestStrategy, Mltests, OandaCSVData
 
 if __name__ == "__main__":
+    tframes = dict(
+        minutes=bt.TimeFrame.Minutes,
+        daily=bt.TimeFrame.Days,
+        weekly=bt.TimeFrame.Weeks,
+        monthly=bt.TimeFrame.Months)
     cerebro = bt.Cerebro()
 
-    cerebro.addstrategy(TestStrategy)
+    #cerebro.addstrategy(TestStrategy)
 
+    #modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
+    #datapath = os.path.join(modpath, './data/EURUSD=X.csv')
+
+    #data = bt.feeds.YahooFinanceCSVData(
+     #   dataname=datapath,
+      #  fromdate=datetime.datetime(2019,8,24),
+       # todate=datetime.datetime(2020,8,25),
+       # revere=False
+    #)
+        #Add our strategy
+    cerebro.addstrategy(Mltests)
+
+    #get oanda data
     modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
-    datapath = os.path.join(modpath, './data/EURUSD=X.csv')
-
-    data = bt.feeds.YahooFinanceCSVData(
+    datapath = os.path.join(modpath, './data/EUR_USD-2005-2017-D1.csv')
+    
+    data = OandaCSVData(
         dataname=datapath,
-        fromdate=datetime.datetime(2019,8,24),
-        todate=datetime.datetime(2020,8,25),
-        revere=False
-    )
+        fromdate=datetime.datetime(2005, 1, 1),
+        todate=datetime.datetime(2006, 1, 1))
 
     cerebro.adddata(data)
 
@@ -31,7 +47,9 @@ if __name__ == "__main__":
     cerebro.addsizer(bt.sizers.FixedSize, stake=10)
 
     # Set the commission - 0.1%
-    cerebro.broker.setcommission(commission=0.001)
+    # cerebro.broker.setcommission(commission=0.001)
+
+    cerebro.broker.setcommission(leverage=50)
 
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
--- strategies/tests.py
+++ strategies/tests.py
@@ -101,3 +101,103 @@ class TestStrategy(bt.Strategy):
 
                 # Keep track of the created order to avoid a 2nd order
                 self.order = self.sell()
+
+class Mltests(bt.Strategy):
+    '''
+    This strategy contains some additional methods that can be used to calcuate
+    whether a position should be subject to a margin close out from Oanda.
+    '''
+    params = (('size', 100000),)
+
+    def __init__(self):
+        self.count = 0
+        self.buybars = [1, 150]
+        self.closebars = [100, 250]
+
+    def next(self):
+        bar = len(self.data)
+        self.dt = self.data.datetime.date()
+        if not self.position:
+            if bar in self.buybars:
+                self.cash_before = self.broker.getcash()
+                value = self.broker.get_value()
+                entry = self.buy(size=self.p.size)
+                entry.addinfo(name='Entry')
+        else:
+            '''
+            First check if margin close out conditions are met. If not, check
+            to see if we should close the position through the strategy rules.
+
+            If a close out occurs, we need to addinfo to the order so that we
+            can log it properly later
+            '''
+            mco_result = self.check_mco(
+                value=self.broker.get_value(),
+                margin_used=self.margin
+            )
+
+            if mco_result == True:
+                close = self.close()
+                close.addinfo(name='MCO')
+
+            elif bar in self.closebars:
+                close = self.close()
+                close.addinfo(name='Close')
+
+        self.count += 1
+
+    def notify_trade(self, trade):
+        if trade.isclosed:
+            print('{}: Trade closed '.format(self.dt))
+            print('{}: PnL Gross {}, Net {}\n\n'.format(self.dt,
+                                                round(trade.pnl,2),
+                                                round(trade.pnlcomm,2)))
+
+    def notify_order(self,order):
+        if order.status == order.Completed:
+            ep = order.executed.price
+            es = order.executed.size
+            tv = ep * es
+            leverage = self.broker.getcommissioninfo(self.data).get_leverage()
+            self.margin = abs((ep * es) / leverage)
+
+            if 'name' in order.info:
+                if order.info['name'] == 'Entry':
+                    print('{}: Entry Order Completed '.format(self.dt))
+                    print('{}: Order Executed Price: {}, Executed Size {}'.format(self.dt,ep,es,))
+                    print('{}: Position Value: {} Margin Used: {}'.format(self.dt,tv,self.margin))
+                elif order.info['name'] == 'MCO':
+                    print('{}: WARNING: Margin Close Out'.format(self.dt))
+                else:
+                    print('{}: Close Order Completed'.format(self.dt))
+
+    def check_mco(self, value, margin_used):
+        '''
+        Make a check to see if the margin close out value has fallen below half
+        of the margin used, close the position.
+
+        value: value of the portfolio for a given data. This is essentially the
+        same as the margin close out value which is balance + pnl
+        margin_used: Initial margin
+        '''
+
+        if value < (margin_used /2):
+            return True
+        else:
+            return False
+
+
+class OandaCSVData(bt.feeds.GenericCSVData):
+    params = (
+        ('nullvalue', float('NaN')),
+        ('dtformat', '%Y-%m-%dT%H:%M:%S.%fZ'),
+        ('datetime', 6),
+        ('time', -1),
+        ('open', 5),
+        ('high', 3),
+        ('low', 4),
+        ('close', 1),
+        ('volume', 7),
+        ('openinterest', -1),
+    )
+
